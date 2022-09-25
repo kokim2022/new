f4aeda5d0a6ca590f4c1df614688ed4b0dbb18c8
kokim <kokim.tech2020@gmail.com>
2022-09-25 09:45:28 +0630

    fs

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="tw-flex">
+    <div class="tw-w-1/3 tw-bg-green-500">
+      <log-in />
+    </div>
+    <div class="tw-w-2/3 tw-bg-red-500">2/3</div>
+  </div>
+</template>
+
+<script>
+import LogIn from "src/components/LoginComponent.vue";
+
+export default {
+  name: "MainLayout",
+
+  components: {
+    LogIn,
+  },
+};
+</script>
