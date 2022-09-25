311e0e25a911dced199f0259730b9d4664272f55
kokim <kokim.tech2020@gmail.com>
2022-09-25 10:22:46 +0630

    geneate cv pages added

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="tw-flex">
-    <div class="tw-w-1/3 tw-bg-green-500">
-      <log-in />
+    <div class="tw-w-1/2 tw-bg-green-500">
+      <generate-cv />
     </div>
-    <div class="tw-w-2/3 tw-bg-red-500">2/3</div>
+    <div class="tw-w-1/2 tw-bg-red-500">2/3</div>
   </div>
 </template>
 
 <script>
-import LogIn from "src/components/LoginComponent.vue";
+import GenerateCv from "src/components/GenerateCv.vue";
 
 export default {
   name: "MainLayout",
 
   components: {
-    LogIn,
+    GenerateCv,
   },
 };
 </script>
